da0828096127ca33b5b65c1b6e15a58491e0bbc7
Sean Callaway <seancallaway@gmail.com>
2022-08-01 11:53:38 -0700

    Add test command.

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from logging import getLogger
 from os import environ
+from random import randint
 
 from discord import Intents
 from discord.ext import commands
@@ -26,5 +27,32 @@ async def on_ready():
 @bot.command()
 async def hello(ctx):
     await ctx.send('Hi there!')
+
+
+@bot.command()
+async def test(ctx, pool: int, target: int):
+    if pool < 2 or pool > 5:
+        await ctx.send('[ERROR] Dice pool should be between 2 and 5.')
+        return
+    elif target < 1 or target > 20:
+        await ctx.send('[ERROR] Target must be between 1 and 20.')
+        return
+
+    results = []
+    successes = 0
+    complications = 0
+
+    for _ in range(pool):
+        result = randint(1, 20)
+        if result == 1:
+            successes += 2
+        elif result <= target:
+            successes += 1
+        elif result == 20:
+            complications += 1
+        results.append(result)
+
+    await ctx.send(f'{successes} successes. {complications} complications. [{", ".join(results)}]')
+
 
 bot.run(bot_token)
